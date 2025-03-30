862d2fe2e5dee4d2e4c25d8f03040ea3cd9b1cbf
abrahamuchos <j.abraham29@gmail.com>
2025-03-30 13:56:56 -0400

    Define hero section and define button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-const App = () => (
-  <main>
-    <h1 className='text-5xl text-violet-300 font-bold font-zentry'>
-      Hello World
-    </h1>
+import Hero from "./components/Hero.jsx";
 
+const App = () => (
+  <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <Hero/>
+
+    <div className='pt-96'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, debitis dolor facilis impedit incidunt
+      itaque perferendis quidem soluta. Ab atque culpa, dolor in mollitia nisi odit perspiciatis repellat rerum
+      temporibus!
+    </div>
   </main>
 );
 
