7d122024625682018417b54c4e2d32f16010088c
abrahamuchos <j.abraham29@gmail.com>
2025-07-30 11:13:37 -0400

    Add about us structure

---
+++ src/components/About.jsx
@@ -0,0 +1,61 @@
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function About() {
+
+  useGSAP(()=>{
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#clip',
+        start: 'center center',
+        end: '+=800 center',
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true
+      }
+    });
+
+    clipAnimation.to('.mask-clip-path', {
+      width: '100vw',
+      height: '100vh',
+      borderRadius: '0',
+    })
+  });
+
+
+  return (
+    <>
+      <div id="about" className="w-screen min-h-screen">
+        <div className="relative mb-8 mt36 flex flex-col items-center gap5">
+          <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Zentry</h2>
+          <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+            Disc<b>o</b>ver the world's <br/>
+            l<b>a</b>rgest shared adventure
+          </div>
+
+          <div className="about-subtext">
+            <p>The Game of Game begins-your life, now an epic MMRPG </p>
+            <p>Zentry unites every player from countless games and platforms</p>
+          </div>
+        </div>
+
+        <div id="clip" className='h-dvh w-screen'>
+          <div className="mask-clip-path about-image">
+            <img
+              src="img/about.webp"
+              alt="background-about"
+              className='absolute left-0 top-0 size-full object-cover'
+            />
+          </div>
+        </div>
+      </div>
+
+
+    </>
+  );
+}
+
+
