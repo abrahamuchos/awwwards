51d85c9b4af285770dfdcfc79d10cc0f6cd5275c
abrahamuchos <j.abraham29@gmail.com>
2025-08-04 15:42:08 -0400

    Define feature section and add card component (bento card)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
+import Features from "./components/Features.jsx";
 
 const App = () => (
   <main className='relative min-h-screen w-screen overflow-x-hidden'>
     <Navbar/>
     <Hero/>
     <About/>
+    <Features/>
     <section className='w-full min-h-screen z-0 bg-blue-300'/>
   </main>
 );
