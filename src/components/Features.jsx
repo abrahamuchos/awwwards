614f256a6d2109f0cf1b651aeae53e2a7c05efb0
abrahamuchos <j.abraham29@gmail.com>
2025-08-04 18:43:20 -0400

    Add tilt effect to cards

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,7 @@
+import { TiLocationArrow } from "react-icons/ti";
+
 import BentoCard from "./BentoCard.jsx";
+import BentoTilt from "./BentoTilt.jsx";
 
 export default function Features() {
 
@@ -13,14 +16,75 @@ export default function Features() {
           </p>
         </div>
 
-        <div className="border-hsla relative mb-7 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <BentoTilt className="border-hsla relative mb-7 w-full overflow-hidden rounded-md md:h-[65vh]">
           <BentoCard
             src="videos/feature-1.mp4"
             title={<>radi<b>n</b>t</>}
             description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
             isComingSoon
           />
+        </BentoTilt>
+
+        {/* Cards Grid*/}
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          {/*Zigma Card*/}
+          <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              src="videos/feature-2.mp4"
+              title={<>zig<b>m</b>a</>}
+              description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+              isComingSoon
+            />
+          </BentoTilt>
+          {/*End Zigma Card*/}
+
+          {/*Nexus Card*/}
+          <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="videos/feature-3.mp4"
+              title={<>n<b>e</b>xus</>}
+              description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
+              isComingSoon
+            />
+          </BentoTilt>
+          {/*End Nexus Card*/}
+
+          {/*Azul Card*/}
+          <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title={<>az<b>u</b>l</>}
+              description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+              isComingSoon
+            />
+          </BentoTilt>
+          {/*End Azul Card*/}
+
+          {/*Coming Soon Card*/}
+          <BentoTilt className="bento-tilt_2">
+            <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+              <h1 className='bento-title special-font max-w-64 text-black'>
+                M<b>o</b>re co<b>m</b>ing s<b>o</b>on!
+              </h1>
+              <TiLocationArrow className='m-5 scale-[5] self-end'/>
+            </div>
+          </BentoTilt>
+          {/*End Coming Soon Card*/}
+
+          {/*  Video Card*/}
+          <BentoTilt className="bento-tilt_2">
+            <video
+              src="videos/feature-5.mp4"
+              loop
+              muted
+              autoPlay
+              className="size-full object-cover object-center"
+            />
+          </BentoTilt>
+          {/* End Video Card*/}
         </div>
+        {/* End Cards Grid*/}
+
 
       </div>
     </section>
