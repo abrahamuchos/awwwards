bfd9027eaf28dc3d05d662b833f5a49c18b06e70
abrahamuchos <j.abraham29@gmail.com>
2025-08-06 13:53:10 -0400

    Add text and cta

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
 
 const App = () => (
   <main className='relative min-h-screen w-screen overflow-x-hidden'>
@@ -9,6 +10,7 @@ const App = () => (
     <Hero/>
     <About/>
     <Features/>
+    <Story/>
     <section className='w-full min-h-screen z-0 bg-blue-300'/>
   </main>
 );
---
+++ src/components/Story.jsx
@@ -0,0 +1,108 @@
+import { useRef } from "react";
+import gsap from 'gsap';
+
+import AnimatedTitle from "./AnimatedTitle.jsx";
+import RoundedCorners from "./RoundedCorners.jsx";
+import Button from "./Button.jsx";
+
+export default function Story() {
+  const frameRef = useRef(/**@type {HTMLImageElement|null}*/ null);
+
+  /**
+   * Set the rotation of the frame to 0 when the mouse leaves the frame
+   * @param {MouseEvent} e
+   */
+  const handleMouseLeave = (e) => {
+    if (!frameRef.current) return;
+
+    const element = frameRef.current;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: 0,
+      rotateY: 0,
+      ease: "power1.inOut",
+    });
+
+  }
+
+  /**
+   * Set the rotation of the frame based on the mouse position
+   *
+   * @param {MouseEvent} e
+   */
+  const handleMouseMove = (e) => {
+    if (!frameRef.current) return;
+
+    const {clientX, clientY} = e;
+    const element = frameRef.current;
+    const rect = element.getBoundingClientRect();
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerX) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX: rotateX,
+      rotateY: rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  }
+
+  return (
+    <section id='story' className='min-h-dvh w-screen bg-black text-blue-50'>
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+
+        <div className="relative size-full">
+          <AnimatedTitle
+            title={"The st<b>o</b>ry of <br/> a hidden real<b>m</b>"}
+            sectionId='#story'
+            className='mt-5 pointer-events-none mix-blend-difference relative z-10'
+          />
+
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                  className='object-contain'
+                  src="/img/entrance.webp"
+                  alt="entrance"/>
+              </div>
+            </div>
+            <RoundedCorners/>
+          </div>
+        </div>
+
+        {/*Info and CTA*/}
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar. Discover its secrets and shape your fate
+              amidst infinite opportunities.
+            </p>
+            <Button id='realm-button' text='Discover prologue' className='mt-5'/>
+          </div>
+        </div>
+        {/*End Info and CTA*/}
+
+      </div>
+
+
+    </section>
+  );
+}
+
+
+
